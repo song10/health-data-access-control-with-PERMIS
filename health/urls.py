230537631234fd33bff1348d11cdd3413a32a441
song10 <song10@gmail.com>
2011-06-20 19:03:50 +0800

    first commit

---
+++ health/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^health/', include('health.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^his1/', include('health.his1.urls')),
+)
