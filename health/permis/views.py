e9f255b074255449e15556ac477bb61a62760276
song10 <song10@gmail.com>
2011-06-27 19:07:08 +0800

    add permis app

--- health/permis/views.py
+++ health/permis/views.py
@@ -6,9 +6,15 @@ from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm
 
+from suds.client import Client
+
 from form.request import *
 from form.models import *
 from center.models import *
 
 def bridge (request):
+	url = "http://localhost:1104/axis2/services/AuthzService?wsdl" 
+	client = Client(url)
+	client.service.XACMLAuthzRequest(__inject={'msg':message.encode()})
+	recvdata = client.last_received() 
 	pass
